08c935755676f9468bf7186f2ed2cb6d7656959d
DevasDiCain <peperodriguezfdz@hotmail.com>
2021-05-04 20:08:45 +0200

    -Punto de referencia

--- index.php
+++ index.php
@@ -36,6 +36,7 @@ if(isset($_SESSION["controlLogin"]))
 			include("registro.php");
 		}
 					}// AQUÍ DENTRO VA LA LOGICA DE LA  SESION
+					//  POR AQUÍ LO HE DEJADOOOOOOOOOOOOOOOO
 ?>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
